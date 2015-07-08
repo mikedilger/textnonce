a245cc6e1771ffe96b6951a6f702c20214d677b1
Mike Dilger <mike@mikedilger.com>
2015-07-09 03:37:15 +1200

    doc update

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['textnonce'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='textnonce/struct.TextNonce.html' title='textnonce::TextNonce'>TextNonce</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
