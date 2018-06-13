b5615f50281690394492f9215707103095a36325
Mike Dilger <mike@optcomp.nz>
2018-06-14 09:49:38 +1200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,22 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-#![cfg_attr(feature="clippy", feature(plugin))]
-#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
 
-extern crate rand;
-extern crate chrono;
-extern crate byteorder;
 extern crate base64;
+extern crate byteorder;
+extern crate chrono;
+extern crate rand;
 #[cfg(feature = "serde")]
 #[macro_use]
 extern crate serde_derive;
 
+use byteorder::{LittleEndian, WriteBytesExt};
+use rand::{OsRng, Rng};
 use std::fmt;
 use std::io::Cursor;
-use rand::{OsRng,Rng};
 use std::ops::Deref;
-use byteorder::{LittleEndian, WriteBytesExt};
 /// A nonce is a cryptographic concept of an arbitrary number that is never used more than once.
 ///
 /// `TextNonce` is a nonce because the first 16 characters represents the current time, which
@@ -32,11 +32,10 @@ use byteorder::{LittleEndian, WriteBytesExt};
 ///
 /// It is also text-based, using only characters in the base64 character set.
 #[derive(Clone, PartialEq, Debug, Default)]
-#[cfg_attr(feature="serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct TextNonce(pub String);
 
 impl TextNonce {
-
     /// Generate a new `TextNonce` with 16 characters of time and 16 characters of
     /// randomness
     pub fn new() -> TextNonce {
@@ -46,28 +45,34 @@ impl TextNonce {
     /// Generate a new `TextNonce`. `length` must be at least 16, and divisible by 4.
     /// The first 16 characters come from the time component, and all characters
     /// after that will be random.
-    pub fn sized(length: usize) -> Result<TextNonce,String> {
+    pub fn sized(length: usize) -> Result<TextNonce, String> {
         TextNonce::sized_configured(length, base64::STANDARD)
     }
 
     /// Generate a new `TextNonce` using the `URL_SAFE` variant of base64 (using '_' and '-')
     /// `length` must be at least 16, and divisible by 4.  The first 16 characters come
     /// from the time component, and all characters after that will be random.
-    pub fn sized_urlsafe(length: usize) ->  Result<TextNonce,String> {
+    pub fn sized_urlsafe(length: usize) -> Result<TextNonce, String> {
         TextNonce::sized_configured(length, base64::URL_SAFE)
     }
 
     /// Generate a new `TextNonce` specifying the Base64 configuration to use.
     /// `length` must be at least 16, and divisible by 4.  The first 16 characters come
     /// from the time component, and all characters after that will be random.
-    pub fn sized_configured(length: usize, config: base64::Config) -> Result<TextNonce,String> {
-        if length<16 { return Err("length must be >= 16".to_owned()); }
-        if length % 4 != 0 { return Err("length must be divisible by 4".to_owned()); }
+    pub fn sized_configured(length: usize, config: base64::Config) -> Result<TextNonce, String> {
+        if length < 16 {
+            return Err("length must be >= 16".to_owned());
+        }
+        if length % 4 != 0 {
+            return Err("length must be divisible by 4".to_owned());
+        }
 
         let bytelength: usize = (length / 4) * 3;
 
         let mut raw: Vec<u8> = Vec::with_capacity(bytelength);
-        unsafe { raw.set_len(bytelength); }
+        unsafe {
+            raw.set_len(bytelength);
+        }
 
         // Get the first 12 bytes from the current time
         {
@@ -83,11 +88,11 @@ impl TextNonce {
         // Get the last bytes from random data
         match OsRng::new() {
             Ok(mut g) => g.fill_bytes(&mut raw[12..bytelength]),
-            Err(_) => ::rand::thread_rng().fill_bytes(&mut raw[12..bytelength])
+            Err(_) => ::rand::thread_rng().fill_bytes(&mut raw[12..bytelength]),
         };
 
         // base64 encode
-        Ok(TextNonce(base64::encode_config( &raw, config )))
+        Ok(TextNonce(base64::encode_config(&raw, config)))
     }
 
     pub fn into_string(self) -> String {
@@ -129,36 +134,37 @@ mod tests {
             assert_eq!(s.len(), 32);
 
             // Verify their character content
-            assert_eq!( s.chars()
-                        .filter(|x| { x.is_digit(10) || x.is_alphabetic() || *x=='+' || *x=='/' })
-                        .count(),
-                        32 );
+            assert_eq!(
+                s.chars()
+                    .filter(|x| x.is_digit(10) || x.is_alphabetic() || *x == '+' || *x == '/')
+                    .count(),
+                32
+            );
 
             // Add to the map
             map.insert(s);
         }
-        assert_eq!( map.len(), 100 );
+        assert_eq!(map.len(), 100);
     }
 
     #[test]
     fn sized() {
         let n = TextNonce::sized(48);
-        assert!( n.is_ok() );
+        assert!(n.is_ok());
         let TextNonce(s) = n.unwrap();
         assert_eq!(s.len(), 48);
 
         let n = TextNonce::sized(47);
-        assert!( n.is_err() );
+        assert!(n.is_err());
         let n = TextNonce::sized(12);
-        assert!( n.is_err() );
+        assert!(n.is_err());
     }
 
     #[cfg(feature = "serde")]
     #[test]
     fn serde() {
         let n = TextNonce::sized(48);
-        let serialized = bincode::serialize(&n, bincode::Infinite)
-            .unwrap();
+        let serialized = bincode::serialize(&n, bincode::Infinite).unwrap();
         let deserialized = bincode::deserialize(&serialized).unwrap();
         assert_eq!(n, deserialized);
     }
