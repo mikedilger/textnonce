2e6e26598c15f587b062ea57467518fdd101af0d
Mike Dilger <mike@optcomp.nz>
2020-10-02 07:29:18 +1300

    line up comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2015-2016 textnonce Developers
+// Copyright 2015-2020 textnonce Developers
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
@@ -10,10 +10,11 @@
 //!
 //! `TextNonce` is a nonce because the first 16 characters represents the current
 //! time, which will never have been generated before, nor will it be generated
-//! again, across the period of time in which Timespec is valid.
+//! again, across the period of time in which a `Timespec` (or `std::time::Duration`,
+//! counting from the UNIX epoch) is valid.
 //!
-//! `TextNonce` additionally includes bytes of randomness, making it difficult
-//! to predict. This makes it suitable to be used for a session ID.
+//! `TextNonce` additionally includes bytes of randomness, making it difficult to
+//! predict. This makes it suitable to be used for a session ID.
 //!
 //! It is also text-based, using only characters in the base64 character set.
 //!
@@ -33,16 +34,21 @@ use std::io::Cursor;
 use std::ops::Deref;
 use std::io::Write;
 
-/// A nonce is a cryptographic concept of an arbitrary number that is never used more than once.
+/// A nonce is a cryptographic concept of an arbitrary number that is never used
+/// more than once.
 ///
-/// `TextNonce` is a nonce because the first 16 characters represents the current time, which
-/// will never have been generated before, nor will it be generated again, across the period of
-/// time in which a `Timespec` (or `std::time::Duration`, counting from the UNIX epoch) is valid.
+/// `TextNonce` is a nonce because the first 16 characters represents the current
+/// time, which will never have been generated before, nor will it be generated
+/// again, across the period of time in which a `Timespec` (or `std::time::Duration`,
+/// counting from the UNIX epoch) is valid.
 ///
-/// `TextNonce` additionally includes bytes of randomness, making it difficult to predict.
-/// This makes it suitable to be used for session IDs.
+/// `TextNonce` additionally includes bytes of randomness, making it difficult to
+/// predict. This makes it suitable to be used for session IDs.
 ///
 /// It is also text-based, using only characters in the base64 character set.
+///
+/// Various length `TextNonce`es may be generated.  The minimum length is 16
+/// characters, and lengths must be evenly divisible by 4.
 #[derive(Clone, PartialEq, Debug, Default)]
 #[cfg_attr(feature ="serde", derive(Serialize, Deserialize))]
 pub struct TextNonce(pub String);
